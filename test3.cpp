86954f6e31b510012d1fc4d6dd2a57b1a1729efc
peNtHlsh <chaplygin-p@list.ru>
2024-06-12 13:06:36 +0300

    Убрал тесты

--- test3.cpp
+++ test3.cpp
@@ -17,20 +17,5 @@ int main() {
     // Тест 5: Одна строка пустая
     assert(areEqual("Hello", "") == false);
 
-    // Тест 6: Обе строки пустые
-    assert(areEqual("", "") == true);
-
-    // Тест 7: Одна строка NULL
-    assert(areEqual("Hello", nullptr) == false);
-
-    // Тест 8: Обе строки NULL
-    assert(areEqual(nullptr, nullptr) == false);
-
-    // Тест 9: Строки с одинаковым началом, но разными концами
-    assert(areEqual("HelloWorld", "Helloworld") == true); // Исправлено на true
-
-    // Тест 10: Строки с символами разных регистров
-    assert(areEqual("HeLLo", "hElLo") == true);
-
     return 0;
 }
